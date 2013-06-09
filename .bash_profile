ee54d2ad18c3871dafe2cb18996899d46f4e2ce6
Jeffrey Kao <jeffthestampede@gmail.com>
2013-06-08 23:23:59 -0700

    initial commit

---
+++ .bash_profile
@@ -0,0 +1,9 @@
+export PATH="/usr/local/bin:/usr/local/sbin:~/bin:$PATH"
+export NODE_PATH="/usr/local/lib/node_modules"
+export PATH="$PATH:/usr/local/share/npm/bin"
+
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+
+PATH=$PATH:$HOME/.rvm/bin
+
+export EDITOR="subl -w"
---
+++ fish/config.fish
@@ -0,0 +1,13 @@
+set -g fish_greeting ' __  __     __          __     ______     ______   ______
+/\ \_\ \   /\ \        /\ \   /\  ___\   /\  ___\ /\  ___\
+\ \  __ \  \ \ \      _\_\ \  \ \  __\   \ \  __\ \ \  __\
+ \ \_\ \_\  \ \_\    /\_____\  \ \_____\  \ \_\    \ \_\
+  \/_/\/_/   \/_/    \/_____/   \/_____/   \/_/     \/_/
+                                                           '
+set EDITOR subl -w
+set NODE_PATH /usr/local/lib/node_modules
+set PATH /usr/local/bin $PATH
+set PATH /usr/local/sbin $PATH
+set PATH /usr/local/share/npm/bin $PATH
+set PATH $HOME/.rvm/bin $PATH
+set PATH $HOME/bin $PATH
